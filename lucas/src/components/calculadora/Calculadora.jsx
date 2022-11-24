97a26ba022d96b1b410a07f1bbe86b9c91ee2ea0
lucasmoraismnt <RM93267@fiap.com.br>
2022-11-23 21:27:59 -0300

    Criando funcionalidade para calculadora

--- lucas/src/components/calculadora/Calculadora.jsx
+++ lucas/src/components/calculadora/Calculadora.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 export const DivCalc = styled.div`
@@ -7,20 +7,57 @@ export const DivCalc = styled.div`
 
     #fundo{
         position: absolute;
-        background-color: black;
+        background-color: rgba(0,0,0,0.8);
         top: 50%;
         left: 50%;
         transform: translate(-50%,-50%);
         border-radius: 15px;
         padding: 15px;
+        color: #ffffff;
     }
-`
 
+    button{
+        width: 50px;
+        height: 50px;
+        font-size: 25px;
+        cursor: pointer;
+        background-color: rgb(31,31,31);
+        border: none;
+        color: #ffffff;
+        margin: 1px;
+    }
+
+    #zero{
+        width: 106px;
+    }
+
+    button:hover{
+        background-color: black;
+    }
+
+    #resultado{
+        background-color: #ffffff;
+        width: 216px;
+        height: 30px;
+        margin: 5px;
+        color: #000;
+        text-align: right;
+        font-size: 25px;
+    }
+
+`
 export default function Calculadora(){
+
+    const [num, setNum] = useState(0);
+
+    function insertNum(e){
+        setNum(e.target.value);
+    }
+    
     return(
         <DivCalc>
         <div id="fundo">
-            <p id="resultado"></p>
+            <p id="resultado">{num}</p>
             <table>
                 <tr>
                     <td><button>C</button></td>
@@ -30,28 +67,28 @@ export default function Calculadora(){
                 </tr>
 
                 <tr>
-                    <td><button>7</button></td>
-                    <td><button>8</button></td>
-                    <td><button>9</button></td>
+                    <td><button onClick={insertNum} value={7}>7</button></td>
+                    <td><button onClick={insertNum} value={8}>8</button></td>
+                    <td><button onClick={insertNum} value={9}>9</button></td>
                     <td><button>X</button></td>
                 </tr>
 
                 <tr>
-                    <td><button>4</button></td>
-                    <td><button>5</button></td>
-                    <td><button>6</button></td>
+                    <td><button onClick={insertNum} value={4}>4</button></td>
+                    <td><button onClick={insertNum} value={5}>5</button></td>
+                    <td><button onClick={insertNum} value={6}>6</button></td>
                     <td><button>-</button></td>
                 </tr>
                 
                 <tr>
-                    <td><button>1</button></td>
-                    <td><button>2</button></td>
-                    <td><button>3</button></td>
+                    <td><button onClick={insertNum} value={1}>1</button></td>
+                    <td><button onClick={insertNum} value={2}>2</button></td>
+                    <td><button onClick={insertNum} value={3}>3</button></td>
                     <td><button>+</button></td>
                 </tr>
 
                 <tr>
-                    <td colSpan="2"><button>0</button></td>
+                    <td colSpan="2"><button id="zero" onClick={insertNum} value={0}>0</button></td>
                     <td><button>,</button></td>
                     <td><button>=</button></td>
                 </tr>
