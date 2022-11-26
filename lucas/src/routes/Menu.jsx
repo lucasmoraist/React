72041e0ce4de090f6afc0d0137732b8e27c02c56
lucasmoraismnt <RM93267@fiap.com.br>
2022-11-25 21:07:21 -0300

    Criando landing page do site

--- lucas/src/routes/Menu.jsx
+++ lucas/src/routes/Menu.jsx
@@ -3,27 +3,63 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.div`
-    a{
-        text-decoration: none;
-        background-color: rgba(0, 42, 255,0.4);
-        color: #fff;
-        margin-left: 10px;
-        margin-right: 30px;
-        border-radius: 30px;
-        padding: 3px;
+    div{
+        position: absolute;
+        width: 430px;
+        height: 976px;
+        left: 0px;
+        top: -15px;
+        background: linear-gradient(180deg, #000000 0%, rgba(255, 255, 255, 0.989583) 0.01%, #686868 36.98%, #000000 100%);
     }
 
-    a:hover{
-        background-color: #fff;
-        color: rgb(0, 42, 255);
+    #home{
+        box-sizing: border-box;
+
+        position: absolute;
+        width: 430px;
+        height: 48.21px;
+        left: 0px;
+        top: 278.18px;
+
+        background: #000000;
+        color: #FFFFFF;
+        
+        text-decoration: none;
+        text-align: center;
+
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 45px;
+    }
+
+    #calculadora{
+        box-sizing: border-box;
+
+        position: absolute;
+        width: 430px;
+        height: 48.21px;
+        left: 0px;
+        top: 333.27px;
+
+        background: #000000;
+        color: #FFFFFF;
+        
+        text-decoration: none;
+        text-align: center;
+
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 45px;
     }
 `
 
 export default function Menu(){
     return(
         <Nav>
-            <Link to="/">Home</Link>
-            <Link to="/calculadora">Calculadora</Link>
+            <div>
+                <Link id="home" to="/">Home</Link>
+                <Link id="calculadora" to="/calculadora">Calculadora</Link>
+            </div>
         </Nav>
     )
 }
